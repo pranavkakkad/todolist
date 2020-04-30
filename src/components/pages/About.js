63a78835d44dc172373f8e48807cd2865680dd70
pranavkakkad <pranavkakkad@gmail.com>
2020-05-01 01:37:28 +0530

    TODO

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TodoList app. It is part of React crash cpurse</p>
+        </React.Fragment>
+    )
+}
+
+
+export default About;
